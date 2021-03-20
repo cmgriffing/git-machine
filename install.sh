c8d3f613860d8e89521b1a6636114aa29159fddf
Chris Griffing <cmgriffing@gmail.com>
2021-03-19 17:36:29 -0700

    chore: scaffold install script

--- install.sh
+++ install.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+echo "Installing git-machine"
+
+echo "SOME ASCII JAMES BROWN"
+
+# download executable for platform
+
+# scaffold $HOME/.git-machine
+
+# echo instruction for PATH
+
+# echo instructions for aliases
