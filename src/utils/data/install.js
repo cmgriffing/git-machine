1d189558eeda331a7f0b4adcec79a77bfd8ae13f
Chris Griffing <cmgriffing@gmail.com>
2021-03-22 14:50:45 -0700

    fix: improve curl command and rebuild site

--- src/utils/data/install.js
+++ src/utils/data/install.js
@@ -1,6 +1,6 @@
 const data = {
   instructions: `
-curl https://github.com/cmgriffing/git-machine/install.sh | sh
+curl -o- -s https://raw.githubusercontent.com/cmgriffing/git-machine/main/install.sh | bash
   `,
 };
 
